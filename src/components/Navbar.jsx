ae0e995d9789a489ef7f119143bbb0380bd08b0a
mbdullah <mdabdullah162005@gmail.com>
2025-08-04 14:22:36 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const NavMenu = () => {
+  return (
+    <>
+      <li className="text-[#444444] font-semibold text-lg">
+        <Link href={"/"}>Home</Link>
+      </li>
+      <li className="text-[#444444] font-semibold text-lg">
+        <Link href={"/about"}>About</Link>
+      </li>
+      <li className="text-[#444444] font-semibold text-lg">
+        <Link href={"/services"}>Services</Link>
+      </li>
+      <li className="text-[#444444] font-semibold text-lg">
+        <Link href={"/blogs"}>Blogs</Link>
+      </li>
+      <li className="text-[#444444] font-semibold text-lg">
+        <Link href={"/contact"}>Contact</Link>
+      </li>
+    </>
+  );
+};
+const Navbar = () => {
+  return (
+    <div className="navbar max-w-[1320px] mx-auto py-3 lg:py-5">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {
+                NavMenu()
+            }
+          </ul>
+        </div>
+        <Link href={"/"} className="text-xl">
+          <Image src={"/assets/logo.svg"} alt="logo" width={60} height={50} />
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+            {
+                NavMenu()
+            }
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-outline text-[#FF3811] text-lg rounded">Appointment</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+import daisyui from 'daisyui'
+
+export default tailwindConfig = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
